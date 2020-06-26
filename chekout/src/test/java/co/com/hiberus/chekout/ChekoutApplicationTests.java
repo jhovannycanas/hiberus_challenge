1d26493fd594079ea07deec8908b1237b6b13e9b
Jhovanny <jhovannycanas@gmail.com>
2020-06-26 15:46:48 -0500

    project start and basic settings

---
+++ chekout/src/test/java/co/com/hiberus/chekout/ChekoutApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.hiberus.chekout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChekoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
